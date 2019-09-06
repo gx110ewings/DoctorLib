68976b5ab200b85e3ee0b74b85a9df53b5cd1b6a
Zain Bhatti <zain.bhatti@ewings.com>
2019-09-06 11:05:59 +0200

    - test cases fixes and new test case added.

--- src/getAvailabilities.test.js
+++ src/getAvailabilities.test.js
@@ -1,20 +1,32 @@
 import knex from "knexClient";
 import getAvailabilities from "./getAvailabilities";
 
+/**
+ *  Test cases for appointment availability module.
+ */
 describe("getAvailabilities", () => {
   beforeEach(() => knex("events").truncate());
 
   describe("case 1", () => {
     it("test 1", async () => {
-      const availabilities = await getAvailabilities(new Date("2014-08-10"));
-      expect(availabilities.length).toBe(7);
-      for (let i = 0; i < 7; ++i) {
+      const numberOfDays = 7; // can not modify due to total days in week are 7
+      const availabilities = await getAvailabilities(new Date("2014-08-10"), numberOfDays);
+      expect(availabilities.length).toBe(numberOfDays);
+      for (let i = 0; i < numberOfDays; ++i) {
         expect(availabilities[i].slots).toEqual([]);
       }
     });
   });
 
   describe("case 2", () => {
+    it("test 1", async () => {
+      const numberOfDays = 10;
+      const availabilities = await getAvailabilities(new Date("2014-08-10"), numberOfDays);
+      expect(availabilities.length).toBe(0);
+    });
+  });
+
+  describe("case 3", () => {
     beforeEach(async () => {
       await knex("events").insert([
         {
@@ -32,31 +44,32 @@ describe("getAvailabilities", () => {
     });
 
     it("test 1", async () => {
-      const availabilities = await getAvailabilities(new Date("2014-08-10"));
-      expect(availabilities.length).toBe(7);
+      const numberOfDays = 7; // can not modify due to total days in week are 7
+      const availabilities = await getAvailabilities(new Date("2014-08-10"), numberOfDays);
+      expect(availabilities.length).toBe(numberOfDays);
 
       expect(String(availabilities[0].date)).toBe(
-        String(new Date("2014-08-10"))
+          String(new Date("2014-08-10"))
       );
       expect(availabilities[0].slots).toEqual([]);
 
       expect(String(availabilities[1].date)).toBe(
-        String(new Date("2014-08-11"))
+          String(new Date("2014-08-11"))
       );
       expect(availabilities[1].slots).toEqual([
         "9:30",
         "10:00",
         "11:30",
-        "21:00"
+        "12:00"
       ]);
 
       expect(String(availabilities[6].date)).toBe(
-        String(new Date("2014-08-16"))
+          String(new Date("2014-08-16"))
       );
     });
   });
 
-  describe("case 3", () => {
+  describe("case 4", () => {
     beforeEach(async () => {
       await knex("events").insert([
         {
@@ -74,18 +87,94 @@ describe("getAvailabilities", () => {
     });
 
     it("test 1", async () => {
-      const availabilities = await getAvailabilities(new Date("2014-08-10"));
-      expect(availabilities.length).toBe(7);
+      const numberOfDays = 7; // can not modify due to total days in week are 7
+      const availabilities = await getAvailabilities(new Date("2014-08-10"), numberOfDays);
+      expect(availabilities.length).toBe(numberOfDays);
 
       expect(String(availabilities[0].date)).toBe(
-        String(new Date("2014-08-10"))
+          String(new Date("2014-08-10"))
       );
       expect(availabilities[0].slots).toEqual([]);
 
       expect(String(availabilities[1].date)).toBe(
-        String(new Date("2014-08-11"))
+          String(new Date("2014-08-11"))
       );
       expect(availabilities[6].slots).toEqual([]);
+    });
+  });
+
+  describe("case 5", () => {
+    beforeEach(async () => {
+      await knex("events").insert([
+        {
+          kind: "appointment",
+          starts_at: new Date("2014-08-11 10:30"),
+          ends_at: new Date("2014-08-11 11:30")
+        },
+        {
+          kind: "opening",
+          starts_at: new Date("2018-08-04 09:30"),
+          ends_at: new Date("2018-08-04 12:30"),
+          weekly_recurring: true
+        }
+      ]);
+    });
+
+    it("test 1", async () => {
+      const numberOfDays = 7; // can not modify due to total days in week are 7
+      const availabilities = await getAvailabilities(new Date("2014-08-10"), numberOfDays);
+      expect(availabilities.length).toBe(numberOfDays);
+
+      expect(String(availabilities[0].date)).toBe(
+          String(new Date("2014-08-10"))
+      );
+      expect(availabilities[0].slots).toEqual([]);
+
+      expect(String(availabilities[1].date)).toBe(
+          String(new Date("2014-08-11"))
+      );
+      expect(availabilities[6].slots).toEqual([]);
+    });
+  });
+
+  describe("case 6", () => {
+    beforeEach(async () => {
+      await knex("events").insert([
+        {
+          kind: "appointment",
+          starts_at: new Date("2018-08-11 10:30"),
+          ends_at: new Date("2018-08-11 12:30")
+        },
+        {
+          kind: "opening",
+          starts_at: new Date("2018-08-04 09:30"),
+          ends_at: new Date("2018-08-04 12:30"),
+          weekly_recurring: true
+        }
+      ]);
+    });
+
+    it("test 1", async () => {
+      const numberOfDays = 7; // can not modify due to total days in week are 7
+      const availabilities = await getAvailabilities(new Date("2018-08-10"), numberOfDays);
+      expect(availabilities.length).toBe(numberOfDays);
+
+      expect(String(availabilities[0].date)).toBe(
+          String(new Date("2018-08-10"))
+      );
+      expect(availabilities[0].slots).toEqual([]);
+
+      expect(String(availabilities[1].date)).toBe(
+          String(new Date("2018-08-11"))
+      );
+      expect(availabilities[1].slots).toEqual([
+        "9:30",
+        "10:00"
+      ]);
+
+      expect(String(availabilities[6].date)).toBe(
+          String(new Date("2018-08-16"))
+      );
     });
   });
 });
