35e962cd86fd437f97bddb07318acf9e5a68e788
Zain Bhatti <zain.bhatti@ewings.com>
2019-09-06 11:03:23 +0200

    - Sql statement changes, now getting record by slot kind order - check added for event date for current day in loop

--- src/getAvailabilities.js
+++ src/getAvailabilities.js
@@ -1,10 +1,22 @@
 import moment from "moment";
 import knex from "knexClient";
 
-export default async function getAvailabilities(date) {
+/**
+ * Return availability
+ *
+ * @param date
+ * @param numberOfDays
+ *
+ * @return {Promise<[]>}
+ */
+export default async function getAvailabilities(date, numberOfDays = 7) {
+  // Return empty array if numbers of days exceed seven
+  if (numberOfDays > 7) {
+    return [];
+  }
   const availabilities = new Map();
-  for (let i = 0; i < 7; ++i) {
-    const tmpDate = moment(date).add(i, "days");
+  for (let i = 0; i < numberOfDays; ++i) {
+    let tmpDate = moment(date).add(i, "days");
     availabilities.set(tmpDate.format("d"), {
       date: tmpDate.toDate(),
       slots: []
@@ -12,24 +24,26 @@ export default async function getAvailabilities(date) {
   }
 
   const events = await knex
-    .select("kind", "starts_at", "ends_at", "weekly_recurring")
-    .from("events")
-    .where(function() {
-      this.where("weekly_recurring", true).orWhere("ends_at", ">", +date);
-    });
+      .select("kind", "starts_at", "ends_at", "weekly_recurring")
+      .from("events")
+      .orderBy("kind", "desc")
+      .where(function () {
+        this.where("weekly_recurring", true).orWhere("ends_at", ">", +date);
+      });
 
   for (const event of events) {
     for (
-      let date = moment(event.starts_at);
-      date.isBefore(event.ends_at);
-      date.add(30, "minutes")
+        let date = moment(event.starts_at);
+        date.isBefore(event.ends_at);
+        date.add(30, "minutes"
+        )
     ) {
-      const day = availabilities.get(date.format("d"));
-      if (event.kind === "opening") {
+      let day = availabilities.get(date.format("d"));
+      if (event.kind === "opening" && date.isAfter(day.date) === false) {
         day.slots.push(date.format("H:mm"));
       } else if (event.kind === "appointment") {
         day.slots = day.slots.filter(
-          slot => slot.indexOf(date.format("H:mm")) === -1
+            slot => slot.indexOf(date.format("H:mm")) === -1
         );
       }
     }
